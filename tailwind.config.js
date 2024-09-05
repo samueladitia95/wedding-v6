5c8116e3f66966ffb7fa98af527fa05042d04b0e
Samuel Aditia <samueladitia95@gmail.com>
2024-09-05 19:09:43 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				arizona: ['Arizona', ...defaultTheme.fontFamily.sans]
+			}
+		}
 	},
 	plugins: []
 };

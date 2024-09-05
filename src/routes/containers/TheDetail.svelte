4b00138f0c58d0313070f2742968f5c109f1efe3
Samuel Aditia <samueladitia95@gmail.com>
2024-09-05 22:29:18 +0700

    update

--- src/routes/containers/TheDetail.svelte
+++ src/routes/containers/TheDetail.svelte
@@ -36,30 +36,35 @@
 	<div class="container py-28 text-white md:!text-center">
 		<div class="flex flex-col gap-12">
 			<div class="text-4.5xl font-medium">The Details</div>
-			{#each details as detail}
-				<div class="flex flex-col items-start md:!items-center gap-8">
-					<div class="flex flex-col gap-4">
-						<div class="">{detail.name}</div>
-						<div>{detail.time}</div>
-					</div>
+			<div class="flex flex-col xl:!grid xl:!grid-cols-6 gap-12">
+				<div />
+				{#each details as detail}
+					<div class="flex flex-col items-start md:!items-center gap-8 col-span-2">
+						<div class="flex flex-col gap-4">
+							<div class="">{detail.name}</div>
+							<div>{detail.time}</div>
+						</div>
 
-					<div class="flex flex-col gap-2">
-						<div>{detail.location}</div>
-						<div class="text-sm italic font-roman">{detail.address}</div>
-					</div>
+						<div class="flex flex-col gap-2">
+							<div>{detail.location}</div>
+							<div class="text-sm italic font-roman">{detail.address}</div>
+						</div>
 
-					<a class="px-6 py-3 bg-white-54 rounded-full" href={detail.coordinate} target="_blank">
-						Get Direction
-					</a>
-				</div>
-			{/each}
+						<a class="px-6 py-3 bg-white-54 rounded-full" href={detail.coordinate} target="_blank">
+							Get Direction
+						</a>
+					</div>
+				{/each}
+				<div />
+			</div>
 		</div>
 
 		<div class="mt-40 flex flex-col gap-12">
 			<div class="flex flex-col gap-6">
 				<div class="text-4.5xl font-medium">The Attire</div>
 				<div>
-					We kindly encourage our guests to wear <br /> these attire below for our special day
+					We kindly encourage our guests to wear <br class="xl:!hidden" /> these attire below for our
+					special day
 				</div>
 			</div>
 

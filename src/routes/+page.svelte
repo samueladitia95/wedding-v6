6c09e2f7666131aa7bc0db805dc6ffe3372165ef
Samuel Aditia <samueladitia95@gmail.com>
2024-09-05 22:48:07 +0700

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
 	import { pb } from '$lib/pocketbase';
 	import type { PageData } from './$types';
+	import Gallery from './containers/Gallery.svelte';
 	export let data: PageData;
 
 	import Invitation from './containers/Invitation.svelte';
 	import TheDetail from './containers/TheDetail.svelte';
+
+	$: galleries = data.wedding.galleries.map((el: string) => {
+		return pb.getFileUrl(data.wedding, el);
+	});
 </script>
 
 <div>
@@ -13,4 +18,5 @@
 		invImage2={pb.getFileUrl(data.wedding, data.wedding.Invitation_Image_2)}
 	/>
 	<TheDetail />
+	<Gallery galleriesImages={galleries} />
 </div>
---
+++ src/routes/containers/Gallery.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { Marquee } from '@selemondev/svelte-marquee';
+	import '@selemondev/svelte-marquee/dist/style.css';
+
+	export let galleriesImages: string[];
+</script>
+
+<div class="max-h-[560px] h-[560px]">
+	<Marquee fade={false} class="gap-[0px] [--duration:40s] [--gap:0px]" innerClassName="gap-[0px]">
+		{#each galleriesImages as src}
+			<img {src} alt="gallery" class="max-h-[560px] h-[560px] object-cover" />
+		{/each}
+	</Marquee>
+</div>
